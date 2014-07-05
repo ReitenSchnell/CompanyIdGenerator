694b74cc2368ba82afdb52e548fc6a0fe62298da
ReitenSchnell <alina542.1@gmail.com>
2014-07-05 15:35:49 +0300

    adds ApiController

---
+++ FieldsGenerator/Controllers/CalcController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using FieldsGenerator.Logic;
+
+namespace FieldsGenerator.Controllers
+{
+    public class CalcController : ApiController
+    {
+        private readonly Dictionary<string, Func<string>> map = new Dictionary<string, Func<string>>
+            {
+                {"Swedish Company Id", SweCompanyCode.Generate},
+                {"Swedish Tax Number", SweTaxCode.Generate},
+                {"Finnish Company Id", FinCompanyCode.Generate},
+                {"Finnish Tax Number", FinTaxCode.Generate},
+            }; 
+
+        public string Get(string calculationType)
+        {
+            return map.ContainsKey(calculationType) ? map[calculationType]() : string.Empty;
+        }
+         
+    }
+}
